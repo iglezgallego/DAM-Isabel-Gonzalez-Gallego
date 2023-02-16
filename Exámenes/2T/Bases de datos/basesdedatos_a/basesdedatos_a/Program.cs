3d37ee775053c0edddf34d9ea7293df5895df83d
iglezgallego <118050905+iglezgallego@users.noreply.github.com>
2023-02-16 17:16:53 +0100

    examen bases de datos

---
+++ Exámenes/2T/Bases de datos/basesdedatos_a/basesdedatos_a/Program.cs
@@ -0,0 +1,17 @@
+﻿// Informo al usuario sobre el nombre del programa
+Console.WriteLine("Programa de calculadora de velocidad del sonido v.0.1");
+// Le indico al usuario lo que le voy a pedir
+Console.WriteLine("Indica cuántos segundos han pasado desde el trueno hasta el rayo");
+// Almaceno la entrada en una variable
+string segundos = Console.ReadLine();
+// Como la entrada es un string, lo convierto en entero
+int segundosentero = Int32.Parse(segundos);
+// Saco por pantalla una comprobación
+Console.WriteLine("El sonido ha tardado " + segundos + " segundos en llegar");
+// Introduzco como variable la velocidad del sonido
+int velocidadsonido = 343;
+// La distancia es igual a los segundos multiplicado por la velocidad
+int distancia = velocidadsonido * segundosentero;
+// Saco el resultado por pantalla
+Console.WriteLine("La velocidad del sonido es " + velocidadsonido + " metros por segundo");
+Console.WriteLine("La tormenta se encuentra a una distancia de " + distancia + " metros");
---
+++ Exámenes/2T/Bases de datos/basesdedatos_b1/basesdedatos_b1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Informo al usuario del nombre del programa
+Console.WriteLine("Programa ¿Cuanto queda para llegar?");
+// Indico al usuario lo que le voy a pedir
+Console.WriteLine("Indica la velocidad a la que va tu coche en km/h:");
+// Almaceno la entrada en una variable
+string velocidad = Console.ReadLine();
+// Indico al usuario lo que le voy a pedir
+Console.WriteLine("Indica la distancia hasta tu destino en km:");
+// Almaceno la entrada en una variable
+string distancia = Console.ReadLine();
+// Convierto la variable a flotante
+float velocidadfloat = float.Parse(velocidad);
+float distanciafloat = float.Parse(distancia);
+// Saco por pantalla
+Console.WriteLine("La velocidad a la que va tu coche es: " + velocidad + " km/s");
+Console.WriteLine("La distancia hasta tu destino es de: " + distancia + " km");
+// Realizo los cálculos
+float tiempo = distanciafloat / velocidadfloat;
+// Saco el resultado por pantalla
+Console.WriteLine("Quedan " + tiempo + " horas para llegar tu destino");
